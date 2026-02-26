dd4a6f0b8958d3f62a75530d7b0e9dbc41cf3ab6
thiyada-kitt <pthiyada.kittiwithitkun@gmail.com>
2026-02-27 00:15:27 +0700

    Auto create superuser on deploy

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser if it doesn't exist
+python manage.py createsuperuser --noinput || true
