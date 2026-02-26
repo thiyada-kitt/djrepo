edd59b1ab9f9a9662934d6055dddfcbf44e9bdf7
thiyada-kitt <pthiyada.kittiwithitkun@gmail.com>
2026-02-26 23:45:47 +0700

    rewrite build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,3 @@
-# build.sh (ตรวจสอบว่ามีไฟล์นี้อยู่ในโปรเจกต์)
-#!/usr/bin/env bash
-# Exit on error
-set -o errexit
-
-# ติดตั้ง Dependencies
 pip install -r requirements.txt
-
-# รวบรวม Static Files (CSS, JS, Images)
 python manage.py collectstatic --no-input
-
-# อัปเดตโครงสร้างฐานข้อมูล
 python manage.py migrate
